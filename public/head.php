c6aae070e0599e945eb525ad79811ca2acc8ce31
Prakash <prakashk827@gmail.com>
2020-04-04 20:10:18 +0530

    public folder created head file added in public folder
    Signed-off-by: Prakash <prakashk827@gmail.com>

---
+++ public/head.php
@@ -0,0 +1,30 @@
+<head>
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Discover Html5 Template</title>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    
+    <!-- Owl Carousele CSS -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+    
+    <!-- Slicknav CSS -->
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+    
+    <!-- Magnific  Popup CSS -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    
+     <!-- Main CSS -->
+    <link rel="stylesheet" href="style.css">
+    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    
+    <!-- Main JS -->
+    <script src="assets/js/jquery-1.11.3.js"></script>
+</head>
