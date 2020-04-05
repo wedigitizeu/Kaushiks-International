1229c072f869baae3b1e4a2b50a0eca591083d83
Prakash <prakashk827@gmail.com>
2020-04-05 17:58:16 +0530

    separate seo file included.
    Signed-off-by: Prakash <prakashk827@gmail.com>

--- public/head.php
+++ public/head.php
@@ -2,8 +2,9 @@
 <?php include_once("functions/companyInfo.php")?>
 
 <head>
-    <meta charset="UTF-8">    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php include_once("seo.php")?>
+
     <!-- Title -->
     
     
---
+++ public/seo.php
@@ -0,0 +1,6 @@
+<?php
+
+/*Do Not Put Title tag here*/
+?>
+<meta name="Description" content="Kaushik International">
+<meta name="Keywords" content="Kaushik International">
